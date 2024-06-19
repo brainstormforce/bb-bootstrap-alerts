998e12c417c559ae31614abb848278bb4253234e
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-06-19 12:27:47 +0530

    sanitized code

--- bb-bootstrap-alerts-module/includes/frontend.php
+++ bb-bootstrap-alerts-module/includes/frontend.php
@@ -11,12 +11,12 @@
 
 	<!-- Navigation Link -->
 	<?php if($settings->bbn_dropdown_link=='yes'): ?>
-		<a href="<?php echo ($nav_link=$settings->bbn_navigation_link != '')? $nav_link=$settings->bbn_navigation_link : '#' ;?>" target="<?php echo $nav_target=$settings->bbn_navigation_target;?>" >
+		<a href="<?php echo esc_url($nav_link=$settings->bbn_navigation_link != '')? $nav_link=$settings->bbn_navigation_link : '#' ;?>" target="<?php echo esc_attr( $nav_target=$settings->bbn_navigation_target );?>" >
 	<?php endif; ?>
 			<!-- Alert class -->
-			<div class="alert <?php echo $settings->bbn_dropdown_field; ?> fade in" >
+			<div class="alert <?php echo esc_attr( $settings->bbn_dropdown_field ); ?> fade in" >
 				<!-- Close Button -->
-				<?php if( $settings->bbn_closable == 'yes' ): ?>
+				<?php if( esc_attr( $settings->bbn_closable == 'yes' ) ): ?>
 					<span class="close" data-dismiss="alert" aria-label="close"><i class="fa fa-times" aria-hidden="true"></i></span>
 				<?php endif; ?>
 
@@ -25,16 +25,16 @@
 	    			<div class="alert-padding">
 
 	    			<!-- Before text icon -->
-	    			<?php if( $settings->bbn_icon_align == 'before' ): ?>
-						<i class="<?php echo $settings->bbn_icon_field; ?>"></i>
+	    			<?php if( esc_attr( $settings->bbn_icon_align == 'before' ) ): ?>
+						<i class="<?php echo esc_attr( $settings->bbn_icon_field ); ?>"></i>
 					<?php endif; ?>
 
 					<!-- Alert Contents -->
 	    			<?php echo $settings->bbn_textarea_field; ?>
 
 	    			<!-- After text icon -->
-	    			<?php if( $settings->bbn_icon_align == 'after' ): ?>
-	    				<i class="<?php echo $settings->bbn_icon_field; ?>"></i>
+	    			<?php if( esc_attr( $settings->bbn_icon_align == 'after' ) ): ?>
+	    				<i class="<?php echo esc_attr( $settings->bbn_icon_field ); ?>"></i>
 					<?php endif; ?>
 
 					</div>
